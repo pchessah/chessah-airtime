2816040252097dc67b047433cf29e77dd2b6071b
Paul Chesa <paulchesa1@gmail.com>
2023-05-25 14:54:24 +0300

    Created state library

---
+++ libs/state/src/lib/state.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-state',
+  template: `
+    <p>
+      state works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class StateComponent {
+
+}
