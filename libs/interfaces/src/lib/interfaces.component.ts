043a72722ab2be49fb3d57eff23535a0a6f5c72b
Paul Chesa <paulchesa1@gmail.com>
2023-05-25 14:53:26 +0300

    Created interfaces library

---
+++ libs/interfaces/src/lib/interfaces.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-interfaces',
+  template: `
+    <p>
+      interfaces works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class InterfacesComponent {
+
+}
