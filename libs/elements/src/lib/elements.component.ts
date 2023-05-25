7cff757c0c95d95249cf9b73267d7816a16ea405
Paul Chesa <paulchesa1@gmail.com>
2023-05-25 14:55:16 +0300

    Created elements library

---
+++ libs/elements/src/lib/elements.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-elements',
+  template: `
+    <p>
+      elements works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ElementsComponent {
+
+}
