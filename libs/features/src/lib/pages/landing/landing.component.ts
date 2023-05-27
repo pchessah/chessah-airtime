205e912522539f422e2739417e64be64afeac0db
Paul Chesa <paulchesa1@gmail.com>
2023-05-27 12:54:44 +0300

    Minor update to homepage

--- libs/features/src/lib/pages/landing/landing.component.ts
+++ libs/features/src/lib/pages/landing/landing.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "libs/state/src/lib/auth/auth.service";
+import { SubSink } from "subsink";
 
 @Component({
-  selector: 'lib-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  selector: "lib-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.css"],
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit, OnDestroy {
+  private _sbs = new SubSink();
+  isLoggedIn: boolean = false;
+  isLoading: boolean = true;
 
-  constructor(private _router:Router){
+  constructor(private _router: Router, private _authService: AuthService) {}
 
+  ngOnInit(): void {
+    this._sbs.sink = this._authService.userAuthStatus().subscribe((user) => {
+      this.isLoggedIn = user.email.length > 0;
+      this.isLoading = false;
+    });
   }
 
-  goTo(page: "signup" | "login"){
-    this._router.navigateByUrl("/pages/"+page);
+  goTo(page: "signup" | "login" | "dashboard") {
+    this._router.navigateByUrl("/pages/" + page);
   }
 
+  ngOnDestroy(): void {
+    this._sbs.unsubscribe();
+  }
 }
