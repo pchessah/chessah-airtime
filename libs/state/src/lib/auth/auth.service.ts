bd6777709fb2b5d6306fdf4d4077ffa2f994b11a
Paul Chesa <paulchesa1@gmail.com>
2023-08-02 08:27:55 +0300

    Remove unused code

--- libs/state/src/lib/auth/auth.service.ts
+++ libs/state/src/lib/auth/auth.service.ts
@@ -19,20 +19,7 @@ export class AuthService {
     public afAuth: AngularFireAuth, // Inject Firebase auth service
     public router: Router,
     public ngZone: NgZone // NgZone service to remove outside scope warning
-  ) {
-    // /* Saving user data in localstorage when 
-    // logged in and setting up null when logged out */
-    // this.afAuth.authState.subscribe((user) => {
-    //   if (user) {
-    //     this.userData = user;
-    //     localStorage.setItem("user", JSON.stringify(this.userData));
-    //     JSON.parse(localStorage.getItem("user")!);
-    //   } else {
-    //     localStorage.setItem("user", "null");
-    //     JSON.parse(localStorage.getItem("user")!);
-    //   }
-    // });
-  }
+  ) { }
 
   addUserToLocalStorage(user:any){
     this.userData = user;
